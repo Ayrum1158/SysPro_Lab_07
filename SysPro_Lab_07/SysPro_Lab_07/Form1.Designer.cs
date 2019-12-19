f5d725302b23ab861be97fe0c1eaa2230d3df916
Olexandr Korotetskyi <ayrum1158@gmail.com>
2019-12-20 01:52:31 +0200

    Added: Almost finished lab_07 For future: need to implement row deletion and add some more attributes to DB
    Signed-off-by: Olexandr Korotetskyi <ayrum1158@gmail.com>

---
+++ SysPro_Lab_07/SysPro_Lab_07/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SysPro_Lab_07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DBDataGridView = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.libraryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.databaseDataSet = new SysPro_Lab_07.DatabaseDataSet();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.libraryTableAdapter = new SysPro_Lab_07.DatabaseDataSetTableAdapters.LibraryTableAdapter();
+            this.SaveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DBDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DBDataGridView
+            // 
+            this.DBDataGridView.AutoGenerateColumns = false;
+            this.DBDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DBDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.bookDataGridViewTextBoxColumn,
+            this.authorDataGridViewTextBoxColumn});
+            this.DBDataGridView.DataSource = this.libraryBindingSource;
+            resources.ApplyResources(this.DBDataGridView, "DBDataGridView");
+            this.DBDataGridView.Name = "DBDataGridView";
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            resources.ApplyResources(this.idDataGridViewTextBoxColumn, "idDataGridViewTextBoxColumn");
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // bookDataGridViewTextBoxColumn
+            // 
+            this.bookDataGridViewTextBoxColumn.DataPropertyName = "Book";
+            resources.ApplyResources(this.bookDataGridViewTextBoxColumn, "bookDataGridViewTextBoxColumn");
+            this.bookDataGridViewTextBoxColumn.Name = "bookDataGridViewTextBoxColumn";
+            // 
+            // authorDataGridViewTextBoxColumn
+            // 
+            this.authorDataGridViewTextBoxColumn.DataPropertyName = "Author";
+            resources.ApplyResources(this.authorDataGridViewTextBoxColumn, "authorDataGridViewTextBoxColumn");
+            this.authorDataGridViewTextBoxColumn.Name = "authorDataGridViewTextBoxColumn";
+            // 
+            // libraryBindingSource
+            // 
+            this.libraryBindingSource.DataMember = "Library";
+            this.libraryBindingSource.DataSource = this.databaseDataSet;
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "DatabaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // LoadButton
+            // 
+            resources.ApplyResources(this.LoadButton, "LoadButton");
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click_1);
+            // 
+            // libraryTableAdapter
+            // 
+            this.libraryTableAdapter.ClearBeforeFill = true;
+            // 
+            // SaveButton
+            // 
+            resources.ApplyResources(this.SaveButton, "SaveButton");
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.DBDataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DBDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DBDataGridView;
+        private System.Windows.Forms.Button LoadButton;
+        private DatabaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource libraryBindingSource;
+        private DatabaseDataSetTableAdapters.LibraryTableAdapter libraryTableAdapter;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn authorDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ SysPro_Lab_07/SysPro_Lab_07/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+/*
+    Варіант 5. Додаток зберігає інформацію про книги і авторів у бібліотеці.Кожна книга характеризується автором, назвою, видавництвом і роком видання.
+    Реалізувати можливість редагування інформації про авторів, видавництвах і книгах.
+    Реалізувати можливість збереження, вилучення та зміни даних про книги, видавництвах і авторів у таблиці БД.
+*/
+
+namespace SysPro_Lab_07
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        StringBuilder DBpath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DBpath = new StringBuilder();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void LoadButton_Click_1(object sender, EventArgs e)
+        {
+            this.libraryTableAdapter.Fill(this.databaseDataSet.Library);
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            this.libraryTableAdapter.Update(this.databaseDataSet);
+            SqlCommand cmdUpdateDB = new SqlCommand("INSERT INTO Library" + "(ID,FIO,Grupa) Values (@ID,@FIO,@Grupa)");
+)
+        }
+    }
+}
